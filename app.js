2ad89c40e24f71ab916e80ea4039770f60b240e3
liferaam <miticomanly@gmail.com>
2022-12-14 18:42:28 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function pause() {
+  var mario = document.getElementById("mario");
+  var luigi = document.getElementById("luigi");
+  mario.classList.toggle("pauseClass");
+  luigi.classList.toggle("pauseClass");
+}
